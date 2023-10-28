e137742ae546d29e0ca8b0bcdb8cba7efe06d6d7
dongyq <dongyq@Trip.com>
2023-10-28 13:10:32 +0800

    Remove redundant make targets

--- Makefile.core.mk
+++ Makefile.core.mk
@@ -127,9 +127,6 @@ docker-build: docker.higress ## Build and push docker images to registry defined
 
 docker-buildx-push: clean-env docker.higress-buildx
 
-docker-build-base:
-	docker buildx build --no-cache --platform linux/amd64,linux/arm64 -t ${HUB}/base:${HIGRESS_BASE_VERSION} -f docker/Dockerfile.base . --push
-
 export PARENT_GIT_TAG:=$(shell cat VERSION)
 export PARENT_GIT_REVISION:=$(TAG)
 
